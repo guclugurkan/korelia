b38204626ef2cfbc43273bda91327a66fbaa9265
guclugurkan <guclugrkn@gmail.com>
2025-11-16 05:25:53 +0100

    'test33'

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,4 +1,3 @@
-
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 
@@ -15,7 +14,6 @@ import AdminStock from './pages/AdminStock';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Account from './pages/Account';
-
 
 import RequireAuth from "./auth/RequireAuth";
 
@@ -54,68 +52,65 @@ import Confidentialite from './pages/Confidentialite.jsx';
 import MentionsLegales from './pages/MentionsLegales.jsx';
 import AdminReviews from './pages/AdminReviews.jsx';
 
+import { Analytics } from '@vercel/analytics/react';
 
 function App () {
   return (
-    
-
-    
     <SearchProvider>
-    <Router>
-      <Routes>
-        <Route path="/" element= {<HomePage/>} />
-   
-        <Route path='/favoris' element = {<FavoriPage/>} />
-        <Route path='/panier' element = {<PanierPage/>} />
-        <Route path='/blog' element={<Blog/>} />
-        <Route path='/blog/:slug' element={<BlogPost/>} />
+      <Router>
+        {/* Analytics doit être monté une fois au niveau racine */}
+        <Analytics />
 
+        <Routes>
+          <Route path="/" element={<HomePage/>} />
 
-        <Route path='/catalogue' element= {<Catalogue/>} />
-        <Route path='/produit/:slug' element={<Product/>} />
-        <Route path='/checkout' element={<Checkout/>} />
-        <Route path='/merci' element={<Merci/>} />
-        
+          <Route path="/favoris" element={<FavoriPage/>} />
+          <Route path="/panier" element={<PanierPage/>} />
+          <Route path="/blog" element={<Blog/>} />
+          <Route path="/blog/:slug" element={<BlogPost/>} />
 
-        <Route path='/inscription' element={<Register/>}/>
-        <Route path='/connexion' element={<Login/>} />
-        <Route path='/mon-compte' element={<RequireAuth><Account/></RequireAuth>} />
-        
-        <Route path='/forgot-password' element={<ForgotPassword/>} />
-        <Route path='/reset-password' element={<ResetPassword/>} />
+          <Route path="/catalogue" element={<Catalogue/>} />
+          <Route path="/produit/:slug" element={<Product/>} />
+          <Route path="/checkout" element={<Checkout/>} />
+          <Route path="/merci" element={<Merci/>} />
 
-        <Route path='/admin' element={<AdminRoute><Admin/></AdminRoute>} />
-        <Route path='/admin/orders' element={<AdminRoute><AdminOrders/></AdminRoute>} />
-        <Route path='/admin/stock' element={<AdminRoute><AdminStock/></AdminRoute>} />
-        <Route path='/verify-email' element={<VerifyEmail/>} />
-        <Route path='/admin/dashboard' element={<AdminDashboard/>} />
-        <Route path='/mes-commandes' element={<OrdersPage/>}/>
-        <Route path='/pack/:slug' element={<Pack/>} />
-        <Route path='/composer-pack' element={<ComposePack/>} />
-        <Route path='/guide-skincare' element={<SkincareGuide/>} />
-        <Route path='/quiz' element={<QuizSkinType/>} />
-        <Route path='/avantages' element={<Avantages/>} />
-        <Route path='/marques' element={<Brands/>} />
+          <Route path="/inscription" element={<Register/>}/>
+          <Route path="/connexion" element={<Login/>} />
+          <Route path="/mon-compte" element={<RequireAuth><Account/></RequireAuth>} />
 
-        <Route path='/contact' element={<Contact/>} /> 
-        <Route path='/about' element={<About/>} />
-        <Route path='/suivi-commande' element={<SuiviCommande/>} />
-        <Route path='/sitemap' element={<Sitemap/>} />
-        <Route path='/faq' element={<FAQ/>} />
-        <Route path='/livraison' element={<LivraisonRetours/>} />
-        <Route path='/retours' element={<LivraisonRetours/>} />
-        <Route path='/paiement-securise' element={<PaiementSecurise/>} />
-        <Route path='/cgv' element={<CGV_CGU/>} />
-        <Route path='/confidentialite' element={<Confidentialite/>} /> 
-        <Route path='/mentions-legales' element={<MentionsLegales/>} />
+          <Route path="/forgot-password" element={<ForgotPassword/>} />
+          <Route path="/reset-password" element={<ResetPassword/>} />
 
-        <Route path='/admin/reviews' element={<AdminReviews/>} />
-      </Routes>
-    </Router>
+          <Route path="/admin" element={<AdminRoute><Admin/></AdminRoute>} />
+          <Route path="/admin/orders" element={<AdminRoute><AdminOrders/></AdminRoute>} />
+          <Route path="/admin/stock" element={<AdminRoute><AdminStock/></AdminRoute>} />
+          <Route path="/verify-email" element={<VerifyEmail/>} />
+          <Route path="/admin/dashboard" element={<AdminDashboard/>} />
+          <Route path="/mes-commandes" element={<OrdersPage/>}/>
+          <Route path="/pack/:slug" element={<Pack/>} />
+          <Route path="/composer-pack" element={<ComposePack/>} />
+          <Route path="/guide-skincare" element={<SkincareGuide/>} />
+          <Route path="/quiz" element={<QuizSkinType/>} />
+          <Route path="/avantages" element={<Avantages/>} />
+          <Route path="/marques" element={<Brands/>} />
+
+          <Route path="/contact" element={<Contact/>} /> 
+          <Route path="/about" element={<About/>} />
+          <Route path="/suivi-commande" element={<SuiviCommande/>} />
+          <Route path="/sitemap" element={<Sitemap/>} />
+          <Route path="/faq" element={<FAQ/>} />
+          <Route path="/livraison" element={<LivraisonRetours/>} />
+          <Route path="/retours" element={<LivraisonRetours/>} />
+          <Route path="/paiement-securise" element={<PaiementSecurise/>} />
+          <Route path="/cgv" element={<CGV_CGU/>} />
+          <Route path="/confidentialite" element={<Confidentialite/>} /> 
+          <Route path="/mentions-legales" element={<MentionsLegales/>} />
+
+          <Route path="/admin/reviews" element={<AdminReviews/>} />
+        </Routes>
+      </Router>
     </SearchProvider>
-   
   )
 }
-
 
 export default App;
